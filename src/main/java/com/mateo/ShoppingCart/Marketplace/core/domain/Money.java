54ca45ddc943a5aa4c3594459def7b484a2d57d0
mBaenaCh <mateo.baena@hotmail.com>
2021-12-16 16:18:16 -0500

    feature: Money class definition

---
+++ src/main/java/com/mateo/ShoppingCart/Marketplace/core/domain/Money.java
@@ -0,0 +1,13 @@
+package com.mateo.ShoppingCart.Marketplace.core.domain;
+
+import java.math.BigDecimal;
+
+public class Money {
+    private final BigDecimal value;
+    private final Badge badge;
+
+    public Money(BigDecimal value, Badge badge){
+        this.value = value;
+        this.badge = badge;
+    }
+}
