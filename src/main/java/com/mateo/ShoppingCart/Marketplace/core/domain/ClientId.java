773140416b74079d953dd28c9cd3b1547152b6aa
mBaenaCh <mateo.baena@hotmail.com>
2021-12-17 12:10:34 -0500

    Feature: ClientID class validation and specification

--- src/main/java/com/mateo/ShoppingCart/Marketplace/core/domain/ClientId.java
+++ src/main/java/com/mateo/ShoppingCart/Marketplace/core/domain/ClientId.java
@@ -1,6 +1,22 @@
 package com.mateo.ShoppingCart.Marketplace.core.domain;
 
+import java.util.Objects;
 import java.util.UUID;
 
+/*
+*  - value: UUID -> 180 bits -> 36 characters
+* */
 public record ClientId(UUID value) {
+    public ClientId{
+        Objects.requireNonNull(value);
+    }
+    /* Metodo estatico para generar un UUID random*/
+    public static ClientId generateUUID(){
+        return new ClientId(UUID.randomUUID());
+    }
+
+    /* Metodo estatico para generar un UUID en funcion de un String*/
+    public static ClientId getAsString(String value){
+        return new ClientId(UUID.fromString(value));
+    }
 }
