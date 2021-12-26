89115a76927d843e55b87618894c3ed77eaa22b4
mBaenaCh <mateo.baena@hotmail.com>
2021-12-26 12:55:02 -0500

    Feature: testing null pointer exception in the product quantity

---
+++ src/test/java/com/mateo/ShoppingCart/Marketplace/domain/ProductQuantityTest.java
@@ -0,0 +1,22 @@
+package com.mateo.ShoppingCart.Marketplace.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductQuantityTest {
+
+    @Test
+    public void shouldReturnNullPointerExceptionWhenValueIsNull(){
+        //Arrange
+        Integer value = null;
+
+        //Act
+        Executable executable = () -> new ProductQuantity(value);
+
+        //Assert
+        assertThrows(NullPointerException.class, executable);
+    }
+
+}
