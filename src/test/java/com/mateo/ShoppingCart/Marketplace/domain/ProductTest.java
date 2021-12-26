bc234093490d818b03b4f8c3e4a2cd797b3c6639
mBaenaCh <mateo.baena@hotmail.com>
2021-12-26 11:30:06 -0500

    Feature: Adding the last null pointer exception tests

--- src/test/java/com/mateo/ShoppingCart/Marketplace/domain/ProductTest.java
+++ src/test/java/com/mateo/ShoppingCart/Marketplace/domain/ProductTest.java
@@ -1,6 +1,7 @@
 package com.mateo.ShoppingCart.Marketplace.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -8,6 +9,92 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
+
+    @Test
+    public void shouldReturnNullPointerExceptionWhenTheProductIdIsNull(){
+        //Arrange
+        ProductId id = null;
+        ProductName name = new ProductName("Test product name");
+        ProductDescription description = new ProductDescription("Test product description");
+        Badge usd = Badge.USD;
+        Money price = new Money(new BigDecimal(49), usd);
+        ProductQuantity quantity = new ProductQuantity(1);
+
+        //Act
+        Executable executable = () -> new Product(id, name, description, price, quantity);
+        //Assert
+        assertThrows(NullPointerException.class, executable);
+    }
+
+    @Test
+    public void shouldReturnNullPointerExceptionWhenTheProductNameIsNull(){
+        //Arrange
+        UUID uuid = UUID.randomUUID();
+        ProductId id = new ProductId(uuid);
+        ProductName name = null;
+        ProductDescription description = new ProductDescription("Test product description");
+        Badge usd = Badge.USD;
+        Money price = new Money(new BigDecimal(49), usd);
+        ProductQuantity quantity = new ProductQuantity(1);
+
+        //Act
+        Executable executable = () -> new Product(id, name, description, price, quantity);
+        //Assert
+        assertThrows(NullPointerException.class, executable);
+    }
+
+    @Test
+    public void shouldReturnNullPointerExceptionWhenTheProductDescriptionIsNull(){
+        //Arrange
+        UUID uuid = UUID.randomUUID();
+        ProductId id = new ProductId(uuid);
+        ProductName name = new ProductName("Test product name");
+        ProductDescription description = null;
+        Badge usd = Badge.USD;
+        Money price = new Money(new BigDecimal(49), usd);
+        ProductQuantity quantity = new ProductQuantity(1);
+
+        //Act
+        Executable executable = () -> new Product(id, name, description, price, quantity);
+
+        //Assert
+        assertThrows(NullPointerException.class, executable);
+    }
+
+    @Test
+    public void shouldReturnNullPointerExceptionWhenThePriceIsNull(){
+        //Arrange
+        UUID uuid = UUID.randomUUID();
+        ProductId id = new ProductId(uuid);
+        ProductName name = new ProductName("Test product name");
+        ProductDescription description = new ProductDescription("Test product description");
+        Money price = null;
+        ProductQuantity quantity = new ProductQuantity(1);
+
+        //Act
+        Executable executable = () -> new Product(id, name, description, price, quantity);
+
+        //Assert
+        assertThrows(NullPointerException.class, executable);
+    }
+
+    @Test
+    public void shouldReturnNullPointerExceptionWhenTheProductQuantityIsNull(){
+        //Arrange
+        UUID uuid = UUID.randomUUID();
+        ProductId id = new ProductId(uuid);
+        ProductName name = new ProductName("Test product name");
+        ProductDescription description = new ProductDescription("Test product description");
+        Badge usd = Badge.USD;
+        Money price = new Money(new BigDecimal(49), usd);
+        ProductQuantity quantity = null;
+
+        //Act
+        Executable executable = () -> new Product(id, name, description, price, quantity);
+
+        //Assert
+        assertThrows(NullPointerException.class, executable);
+    }
 
     @Test
     public void shouldReturnCheapWhenTheGivenPriceIsBelow50(){
@@ -26,6 +113,7 @@ class ProductTest {
         //Assert
         assertEquals("Cheap", p1.getClasification());
     }
+
     @Test
     public void shouldReturnRegularWhenTheGivenPriceIsAbove50(){
         //Arrange
