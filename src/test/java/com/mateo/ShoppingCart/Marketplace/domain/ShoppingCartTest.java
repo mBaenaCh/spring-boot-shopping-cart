176ea09701b7bcbf52fe78a018bb92b202311143
mBaenaCh <mateo.baena@hotmail.com>
2021-12-27 18:19:26 -0500

    Feature: Setting up the test class

---
+++ src/test/java/com/mateo/ShoppingCart/Marketplace/domain/ShoppingCartTest.java
@@ -0,0 +1,13 @@
+package com.mateo.ShoppingCart.Marketplace.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShoppingCartTest {
+
+    @Test
+    public void shouldReturnACalculatedDiscountGivenAPriceAndDiscount(){
+
+    }
+}
