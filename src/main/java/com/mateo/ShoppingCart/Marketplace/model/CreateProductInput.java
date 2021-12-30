2fbee0d6751fe4b7588cefd8591a850cb87b41f0
mBaenaCh <mateo.baena@hotmail.com>
2021-12-30 12:04:38 -0500

    Feature: defining the create product input

--- src/main/java/com/mateo/ShoppingCart/Marketplace/model/CreateProductInput.java
+++ src/main/java/com/mateo/ShoppingCart/Marketplace/model/CreateProductInput.java
@@ -1,4 +1,14 @@
 package com.mateo.ShoppingCart.Marketplace.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data //Nos trae los metodos getter, setter, toString, etc.
 public class CreateProductInput {
+    private String productId;
+    private String name;
+    private String description;
+    private Number price; //Teniendo en cuenta los valores numericos enteros y flotantes que podriamos recibir en el JSON
+    private Integer quantity;
 }
