4d128f69c26fd774a04351fa30e5ee02680459f2
mBaenaCh <mateo.baena@hotmail.com>
2021-12-30 10:46:50 -0500

    Feature: Defining the interface methods

--- src/main/java/com/mateo/ShoppingCart/Marketplace/repository/ProductRepository.java
+++ src/main/java/com/mateo/ShoppingCart/Marketplace/repository/ProductRepository.java
@@ -1,4 +1,19 @@
 package com.mateo.ShoppingCart.Marketplace.repository;
 
+import com.mateo.ShoppingCart.Marketplace.domain.Product;
+import com.mateo.ShoppingCart.Marketplace.domain.ProductId;
+
+import java.util.List;
+
 public interface ProductRepository {
+    List<Product> getAllProducts();
+
+    Product getProductById(ProductId productId);
+
+    void createProduct(Product product);
+
+    void updateProductById(ProductId productId, Product product);
+
+    void deleteProductById(ProductId productId);
+
 }
