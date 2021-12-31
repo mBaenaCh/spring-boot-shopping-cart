8f6fa6a0bd436ceb5a8c632ec43ab35b7ae902ab
mBaenaCh <mateo.baena@hotmail.com>
2021-12-30 20:43:15 -0500

    Fix: no args constructor in create product input

--- src/main/java/com/mateo/ShoppingCart/Marketplace/model/CreateProductInput.java
+++ src/main/java/com/mateo/ShoppingCart/Marketplace/model/CreateProductInput.java
@@ -2,7 +2,9 @@ package com.mateo.ShoppingCart.Marketplace.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Data //Nos trae los metodos getter, setter, toString, etc.
 public class CreateProductInput {
